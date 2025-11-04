e04f5956a1e325f14dcd51ff3f684d4b33c868e4
seouless <seouless724@gmail.com>
2025-11-04 14:58:15 +0100

    Now theres a real main file

--- src/dllmain.cpp
+++ src/dllmain.cpp
@@ -1,4 +1,8 @@
 #include <Windows.h>
+
+#include "Hooks/HookManager.h"
+#include "Logger/Logging.h"
+#include "Version/VersionChecker.h"
 //#pragma comment (linker, "/export:GetAdaptersAddresses=Proxy.GetAdaptersAddresses,@46")
 //#pragma comment (linker, "/export:GetIfEntry=Proxy.GetIfEntry,@49")
 //#pragma comment (linker, "/export:GetAdaptersInfo=Proxy.GetNetworkParams,@7d")
@@ -6,10 +10,47 @@
 
 void main(HMODULE hMod)
 {
+#ifdef _DEBUG
+    FILE* f;
+    AllocConsole();
+    freopen_s(&f, "CONOUT$", "w", stdout);
+    spdlog::set_default_logger(spdlog::default_logger());
+    spdlog::set_level(spdlog::level::debug);
+    PVZ::LogDebug(L"Initialised Logger!");
+#endif
 
+    auto ver = PVZ::Version::IsSupportedVersionEx();
+	if (ver.has_value())
+	{
+        if (ver.value() == false)
+        {
+            MessageBeep(MB_ICONERROR);
+            MessageBoxW(nullptr, L"Looks like this version is not supported (yet)", L"Error!", MB_OK);
+            Sleep(1000);
+#ifdef _DEBUG
+            spdlog::default_logger().reset();
+            FreeConsole();
+            fclose(f);
+#endif
+            FreeLibraryAndExitThread(hMod, 0);
+        }
+        
+        PVZ::Global::g_pHookManager = std::make_shared<PVZ::Hooks::HookManager>();
+        PVZ::Global::g_pHookManager->HookAll();
 
+        while (!GetAsyncKeyState(VK_F1))
+            Sleep(20);
 
+	}
 
+    PVZ::Global::g_pHookManager->UnHookAll();
+
+#ifdef _DEBUG
+    spdlog::default_logger().reset();
+    FreeConsole();
+    fclose(f);
+#endif
+    FreeLibraryAndExitThread(hMod, 0);
 }
 
 BOOL APIENTRY DllMain( HMODULE hModule,
