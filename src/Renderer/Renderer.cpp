e907a0e5d2601b2fa2c87766b038e0e75b7544d9
seouless <seouless724@gmail.com>
2025-11-04 12:12:14 +0100

    Made renderer class working

--- src/Renderer/Renderer.cpp
+++ src/Renderer/Renderer.cpp
@@ -1,1 +1,100 @@
 #include "Renderer.h"
+
+#include "../../ext/ImGui/imgui.h"
+#include "../../ext/ImGui/imgui_impl_dx11.h"
+#include "../../ext/ImGui/imgui_impl_win32.h"
+
+std::uint8_t PVZ::Render::RendererCounter::m_Counter = 0;
+
+PVZ::Render::Renderer::Renderer(const DXData& Data) : m_DxData(Data)
+{
+	m_IsInit = true;
+	IMGUI_CHECKVERSION();
+	ImGui::CreateContext();
+	auto& io = ImGui::GetIO();
+	io.IniFilename = nullptr;
+	io.LogFilename = nullptr;
+
+	if (!Data.pSwap or !Data.pDevice or !Data.pDeviceCon)
+	{
+		m_IsInit = false;
+	}
+
+	if (!ImGui_ImplWin32_Init(m_DxData.hWnd))
+	{
+		m_IsInit = false;
+	}
+
+	if (!ImGui_ImplDX11_Init(m_DxData.pDevice, m_DxData.pDeviceCon))
+	{
+		m_IsInit = false;
+	}
+
+}
+
+PVZ::Render::Renderer::~Renderer()
+{
+	if (m_IsInit)
+	{
+		m_Menus.clear();
+
+		ImGui_ImplDX11_Shutdown();
+		ImGui_ImplWin32_Shutdown();
+		ImGui::DestroyContext();
+	}
+}
+
+void PVZ::Render::Renderer::Render()
+{
+	if (m_Counter > 0 && m_IsInit)
+	{
+		ImGui_ImplDX11_NewFrame();
+		ImGui_ImplWin32_NewFrame();
+		ImGui::NewFrame();
+
+		for (auto& menus : m_Menus)
+		{
+			if (menus.GetShowMenu())
+				menus.Show();
+		}
+
+
+		ImGui::EndFrame();
+		ImGui::Render();
+		constexpr ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 0.00f);
+		constexpr float clear_color_with_alpha[4] = { clear_color.x * clear_color.w, clear_color.y * clear_color.w, clear_color.z * clear_color.w, clear_color.w };
+		m_DxData.pDeviceCon->OMSetRenderTargets(1, &m_DxData.pRenderTargetView, nullptr);
+		//m_DxData.pDeviceCon->ClearRenderTargetView(m_DxData.pRenderTargetView, clear_color_with_alpha);
+		ImGui_ImplDX11_RenderDrawData(ImGui::GetDrawData());
+	}
+}
+
+void PVZ::Render::Renderer::PushCounter()
+{
+	m_Counter++;
+}
+
+void PVZ::Render::Renderer::PopCounter()
+{
+	m_Counter--;
+}
+
+std::uint8_t PVZ::Render::Renderer::GetCounter() const
+{
+	return m_Counter;
+}
+
+const PVZ::Render::DXData& PVZ::Render::Renderer::GetDXData() const
+{
+	return m_DxData;
+}
+
+PVZ::Render::DXData& PVZ::Render::Renderer::GetDXData()
+{
+	return m_DxData;
+}
+
+std::list<PVZ::Menu>& PVZ::Render::Renderer::GetMenus()
+{
+	return m_Menus;
+}
--- src/Renderer/Renderer.h
+++ src/Renderer/Renderer.h
@@ -1,5 +1,69 @@
 #pragma once
-class Renderer
+#include <cstdint>
+#include <d3d11.h>
+#include <memory>
+#include "../Logger/Logging.h"
+#include "../Menu/Menu.h"
+#pragma comment(lib, "d3d11.lib")
+
+namespace PVZ::Render
 {
-};
+	struct DXData
+	{
+		DXData()
+		{
+		}
+
+		IDXGISwapChain* pSwap = nullptr;
+		ID3D11DeviceContext* pDeviceCon = nullptr;
+		ID3D11Device* pDevice = nullptr;
+		ID3D11RenderTargetView* pRenderTargetView = nullptr;
+		HWND hWnd = nullptr;
+	};
+
+	class RendererCounter
+	{
+	public:
+		RendererCounter()
+		{
+			m_Counter++;
+
+			if (m_Counter > 0)
+			{
+				PVZ::LogCritical(L"Multiple renderers created!");
+			}
+		}
+
+	private:
+		static std::uint8_t m_Counter;
+	};
+
+	class Renderer final : public RendererCounter
+	{
+	public:
+		Renderer(const DXData& Data);
+		~Renderer();
+
+		void Render();
+
+		void PushCounter();
+		void PopCounter();
+		[[nodiscard]] std::uint8_t GetCounter() const;
+
+		[[nodiscard]] const DXData& GetDXData() const;
+		DXData& GetDXData();
+
+		std::list<Menu>& GetMenus();
+	private:
+		std::list<Menu> m_Menus;
+		bool m_IsInit = false;
+		std::uint8_t m_Counter = 0;
+		DXData m_DxData;
+	};
+}
+
+namespace PVZ::Global
+{
+	inline std::unique_ptr<PVZ::Render::Renderer> g_pRenderer = nullptr;
+}
 
