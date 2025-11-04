6485958a9125e72f55ad3e2b352e6b3bf36cdb28
seouless <seouless724@gmail.com>
2025-11-04 14:57:25 +0100

    Added Pattern for testing

--- src/Pattern/Patterns.h
+++ src/Pattern/Patterns.h
@@ -1,8 +1,8 @@
 #pragma once
 namespace PVZ::Pattern
 {
-
-
+	constexpr const char* g_pReloadedGameplayCoinCoinInitialize = "48 89 5C 24 ? 48 89 74 24 ? 57 48 83 EC ? 80 3D ? ? ? ? ? 41 8B F9 0F 29 74 24 ? 48 8B D9";
+	constexpr const char* g_pReloadedGameplayCoinCollect = "40 53 57 41 57 48 83 EC ? 80 3D ? ? ? ? ? 41 0F B6 D8";
 
 
 
